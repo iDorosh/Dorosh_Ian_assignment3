002f073f1c8fd94570fb9907a4f45eaf4d00c42c
Ian Dorosh <iDorosh@Ians-MacBook-Pro.local>
2014-05-22 11:02:33 -0500

    RE-json file
    The reason I deleted the json.js file was because the code would run
    and say that var json1 is missing I simply fixed this by adding the
    line of code so the html file can call it. Just a small mistake and I
    prefer 2 files to keep it a little more organized.

---
+++ json.js
@@ -0,0 +1,26 @@
+//Json Data File
+
+var json1 = {
+	"astronauts": [
+		{
+			"Position": "Captain",
+			"name": "James T. Kirk", 
+			"age": 34
+		},
+		{
+			"Position":"Pilot",
+			"name" :"Jack O'Neil",	
+			"age" : 56
+		},  
+		{    
+		    "Position": "Lead Engineer",
+			"name": "Howard Wolowitz",
+			"age" : 34
+		},
+		{
+			"Postion":"Cheif Medical Officer",
+			"name": "Leanard McCoy",
+			"age": 37
+		}
+	]
+};
